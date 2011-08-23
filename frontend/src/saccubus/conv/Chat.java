d93dd5b59bf06af9ee333d2f2ab5f297dffe27d6
orz- <orz-@c31a3d4d-7098-419a-a5c4-0623780f6bbf>
2011-08-23 19:09:48 +0000

    過去バージョン登録　1.22r3e7
    git-svn-id: svn+ssh://svn.osdn.jp/svnroot/saccubus/tags/1.22r3e7@254 c31a3d4d-7098-419a-a5c4-0623780f6bbf

---
+++ frontend/src/saccubus/conv/Chat.java
@@ -0,0 +1,230 @@
+package saccubus.conv;
+
+import java.io.*;
+
+import saccubus.util.Util;
+
+/**
+ * <p>
+ * タイトル: さきゅばす
+ * </p>
+ * 
+ * <p>
+ * 説明: ニコニコ動画の動画をコメントつきで保存
+ * </p>
+ * 
+ * <p>
+ * 著作権: Copyright (c) 2007 PSI
+ * </p>
+ * 
+ * <p>
+ * 会社名:
+ * </p>
+ * 
+ * @author 未入力
+ * @version 1.0
+ */
+public class Chat {
+	private static final int CMD_LOC_DEF = 0;
+
+	private static final int CMD_LOC_TOP = 1;
+
+	private static final int CMD_LOC_BOTTOM = 2;
+
+	@SuppressWarnings("unused")
+	private static final int CMD_SIZE_MAX = 3;
+
+	private static final int CMD_SIZE_DEF = 0;
+
+	private static final int CMD_SIZE_BIG = 1;
+
+	private static final int CMD_SIZE_SMALL = 2;
+
+	@SuppressWarnings("unused")
+	private static final int COMMENT_FONT_SIZE[] = { 24, // DEF
+			39, // BIG
+			15, // SMALL
+	};
+
+	private static final int CMD_COLOR_DEF = 0;
+
+	private static final int CMD_COLOR_RED = 1;
+
+	private static final int CMD_COLOR_ORANGE = 2;
+
+	private static final int CMD_COLOR_YELLOW = 3;
+
+	private static final int CMD_COLOR_PINK = 4;
+
+	private static final int CMD_COLOR_BLUE = 5;
+
+	private static final int CMD_COLOR_PURPLE = 6;
+
+	private static final int CMD_COLOR_CYAN = 7;
+
+	private static final int CMD_COLOR_GREEN = 8;
+
+	private static final int CMD_COLOR_NICOWHITE = 9;
+
+	private static final int CMD_COLOR_MARINEBLUE = 10;
+
+	private static final int CMD_COLOR_MADYELLOW = 11;
+
+	private static final int CMD_COLOR_PASSIONORANGE = 12;
+
+	private static final int CMD_COLOR_NOBLEVIOLET = 13;
+
+	private static final int CMD_COLOR_ELEMENTALGREEN = 14;
+
+	private static final int CMD_COLOR_TRUERED = 15;
+
+	private static final int CMD_COLOR_BLACK = 16;
+
+	// "date"
+	@SuppressWarnings("unused")
+	private int Date = 0;
+
+	// "mail"
+	private int Color = 0;
+
+	private int Size = 0;
+
+	private int Location = 0;
+
+	// "No"
+	private int No = 0;
+
+	// "user_id"
+	@SuppressWarnings("unused")
+	private int UserID = 0;
+
+	// "vpos"
+	private int Vpos = 0;
+
+	private String Comment = "";
+
+	public Chat() {
+	}
+
+	public void setDate(String date_str) {
+		Date = Integer.parseInt(date_str);
+		// System.out.println("date:" + date_str);
+	}
+
+	public void setMail(String mail_str) {
+		// System.out.println("mail:" + mail_str);
+		Color = CMD_COLOR_DEF;
+		Size = CMD_SIZE_DEF;
+		Location = CMD_LOC_DEF;
+		if (mail_str == null) {
+			return;
+		}
+		String element[] = mail_str.split(" ");
+		for (int i = 0; i < element.length; i++) {
+			String str = element[i].toLowerCase();
+			/* ロケーション */
+			if (str.equals("ue")) {
+				Location = CMD_LOC_TOP;
+			} else if (str.equals("shita")) {
+				Location = CMD_LOC_BOTTOM;
+			} else if (str.equals("big")) {
+				Size = CMD_SIZE_BIG;
+			} else if (str.equals("small")) {
+				Size = CMD_SIZE_SMALL;
+			} else if (str.equals("red")) {
+				Color = CMD_COLOR_RED;
+			} else if (str.equals("orange")) {
+				Color = CMD_COLOR_ORANGE;
+			} else if (str.equals("yellow")) {
+				Color = CMD_COLOR_YELLOW;
+			} else if (str.equals("pink")) {
+				Color = CMD_COLOR_PINK;
+			} else if (str.equals("blue")) {
+				Color = CMD_COLOR_BLUE;
+			} else if (str.equals("purple")) {
+				Color = CMD_COLOR_PURPLE;
+			} else if (str.equals("cyan")) {
+				Color = CMD_COLOR_CYAN;
+			} else if (str.equals("green")) {
+				Color = CMD_COLOR_GREEN;
+			} else if (str.equals("niconicowhite") || str.equals("white2")) {
+				Color = CMD_COLOR_NICOWHITE;
+			} else if (str.equals("arineblue") || str.equals("blue2")) {
+				Color = CMD_COLOR_MARINEBLUE;
+			} else if (str.equals("madyellow") || str.equals("yellow2")) {
+				Color = CMD_COLOR_MADYELLOW;
+			} else if (str.equals("passionorange") || str.equals("orange2")) {
+				Color = CMD_COLOR_PASSIONORANGE;
+			} else if (str.equals("nobleviolet") || str.equals("purple2")) {
+				Color = CMD_COLOR_NOBLEVIOLET;
+			} else if (str.equals("elementalgreen") || str.equals("green2")) {
+				Color = CMD_COLOR_ELEMENTALGREEN;
+			} else if (str.equals("truered") || str.equals("red2")) {
+				Color = CMD_COLOR_TRUERED;
+			} else if (str.equals("black")) {
+				Color = CMD_COLOR_BLACK;
+			} else if (str.startsWith("#")){
+				// TODO
+				/*
+				Color = simulateColor16(str.substr(1));	
+				*/
+			} else {
+				// System.out.println("Unknown command:" + str);
+			}
+		}
+	}
+
+	public void setNo(String no_str) {
+		try {
+			No = Integer.parseInt(no_str);
+		} catch (Exception e) {
+			No = -1;
+		}
+		// System.out.println("no:" + no_str);
+	}
+
+	public void setUserID(String user_id_str) {
+		// System.out.println("user_id:" + user_id_str);
+		try {
+			UserID = Integer.parseInt(user_id_str);
+		} catch (Exception e) {
+			UserID = -1;
+		}
+	}
+
+	public void setVpos(String vpos_str) {
+		// System.out.println("vpos:" + vpos_str);
+		try {
+			Vpos = Integer.parseInt(vpos_str);
+		} catch (Exception e) {
+			Vpos = -1;
+		}
+
+	}
+
+	public void setComment(String com_str) {
+		// System.out.println("Comment[" + com_str.length() + "]:" + com_str);
+		Comment += com_str;
+	}
+
+	public void write(OutputStream os) throws IOException {
+		byte[] a = {  };
+		try {
+			a = (Comment + "\0").getBytes("UnicodeLittleUnmarked");
+		} catch (UnsupportedEncodingException ex) {
+			ex.printStackTrace();
+		}
+		Util.writeInt(os, No);
+		Util.writeInt(os, Vpos);
+		Util.writeInt(os, Location);
+		Util.writeInt(os, Size);
+		Util.writeInt(os, Color);
+		Util.writeInt(os, a.length);
+		try {
+			os.write(a);
+		} catch (IOException ex1) {
+			ex1.printStackTrace();
+		}
+	}
+
+}
---
+++ frontend/src/saccubus/util/Cws2Fws.java
@@ -0,0 +1,174 @@
+package saccubus.util;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.InflaterInputStream;
+
+//import saccubus.Converter;
+
+
+/**
+ *
+ * @author yuki
+ */
+public class Cws2Fws {
+
+	private static final String CWS = "CWS";
+	private static final String FWS = "FWS";
+	private static final String TMP_FWS = "fws_tmp.swf";
+
+	public Cws2Fws(){
+	}
+	/**
+	 * 圧縮SWFかどうか判定する.
+	 * @param file 判定対象.
+	 * @return 圧縮SWFであればtrue.
+	 */
+	public static boolean isCws(File file) {
+		BufferedInputStream bis = null;
+		try {
+			bis = new BufferedInputStream(new FileInputStream(file));
+			byte header[] = new byte[CWS.length()];
+			bis.read(header, 0, header.length);
+			if (CWS.equals(new String(header))) {
+				return true;
+			}
+			/**
+			 * not modefy, same as 1.22r3e2 = 1.22r3
+			 *
+			if (FWS.equals(new String(header))) {
+				return true;
+			}
+			*/
+		} catch (IOException ex) {
+			log("Cws2Fws:" + ex.getMessage());
+		} finally {
+			if (bis != null) {
+				try {
+					bis.close();
+				} catch (IOException ex) {
+					log("Cws2Fws:" + ex.getMessage());
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 圧縮SWFを展開する.
+	 * @param in 展開対象.
+	 * @return 展開後のファイル. 対象が圧縮SWFでなければnull.
+	 */
+	public static File createFws(File in) {
+		boolean wasCws = false;
+		if (!isCws(in)) {
+			return null;
+		}
+		File out = new File(TMP_FWS);
+		InputStream is = null;
+		InputStream iis = null;
+		OutputStream os = null;
+		try {
+			final int headerSize = 8;
+			byte header[] = new byte[headerSize];
+			is = new FileInputStream(in);
+			int size = 0;
+			while (size < headerSize) {
+				int read = is.read(header, size, headerSize);
+				if (read < 0) {
+					throw new IOException("ファイルフォーマット不正");
+				}
+				size += read;
+			}
+			if (new String(header, 0, CWS.length()).equals(CWS)) {
+				wasCws = true;
+			}
+			header[0] = "F".getBytes()[0]; // CWS -> FWS
+
+			os = new BufferedOutputStream(new FileOutputStream(out));
+			os.write(header);
+
+			byte buffer[] = new byte[1024 * 1024];
+			if (wasCws){
+				iis = new InflaterInputStream(is);
+			} else {
+				iis = is;
+			}
+			while (true) {
+				int rs = iis.read(buffer);
+				if (rs < 0) {
+					break;
+				}
+				os.write(buffer, 0, rs);
+			}
+			return out;
+		} catch (IOException ex) {
+			log("Cws2Fws:" + ex.getMessage());
+		} finally {
+			if (is != null) {
+				try {
+					is.close();
+				} catch (IOException ex) {
+					log("Cws2Fws:" + ex.getMessage());
+				}
+			}
+			if (iis != null) {
+				try {
+					iis.close();
+				} catch (IOException ex) {
+					log("Cws2Fws:" + ex.getMessage());
+				}
+			}
+			if (os != null) {
+				try {
+					os.close();
+				} catch (IOException ex) {
+					log("Cws2Fws:" + ex.getMessage());
+				}
+			}
+		}
+		return null;
+	}
+/*
+	public static File createFws(File in, Converter converter) throws IOException {
+		File out = createFws(in);
+		if (out != null){
+			out = converter.nmm2avi(out);
+		}
+		return out;
+	}
+*/
+	private static void log(String msg){
+		System.out.println(msg);
+	}
+
+	public static boolean isFws(File file) {
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(file);
+			byte header[] = new byte[FWS.length()];
+			fis.read(header, 0, header.length);
+			if (FWS.equals(new String(header))) {
+				return true;
+			}
+		} catch (IOException ex) {
+			log("Cws2Fws:" + ex.getMessage());
+		} finally {
+			if (fis != null) {
+				try {
+					fis.close();
+				} catch (IOException ex) {
+					log("Cws2Fws:" + ex.getMessage());
+				}
+			}
+		}
+		return false;
+	}
+
+}
