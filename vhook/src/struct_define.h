e0a8556007ff96dfa27306ffd7f3eb1b6b21d36a
orz- <orz-@c31a3d4d-7098-419a-a5c4-0623780f6bbf>
2011-10-03 01:45:58 +0000

    1.26.α１ 不適切な動画に関する修正ブランチ
    git-svn-id: svn+ssh://svn.osdn.jp/svnroot/saccubus/branches/1.26.beta.1@296 c31a3d4d-7098-419a-a5c4-0623780f6bbf

---
+++ vhook/src/struct_define.h
@@ -0,0 +1,12 @@
+#ifndef STRUCT_DEFINE_H_
+#define STRUCT_DEFINE_H_
+
+typedef struct DATA DATA;
+typedef struct CHAT CHAT;
+typedef struct CHAT_ITEM CHAT_ITEM;
+typedef struct CHAT_SLOT CHAT_SLOT;
+typedef struct CHAT_SLOT_ITEM CHAT_SLOT_ITEM;
+typedef struct CHAT_SET CHAT_SET;
+typedef struct CDATA CDATA;
+
+#endif /*STRUCT_DEFINE_H_*/
