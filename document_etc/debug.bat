eba6cb6909b352e523ac5523a27d9854a496fd89
ledyba <ledyba@c31a3d4d-7098-419a-a5c4-0623780f6bbf>
2012-05-12 12:35:32 +0000

    バイナリ公開用に使う。一旦削除。
    git-svn-id: svn+ssh://svn.osdn.jp/svnroot/saccubus/trunk@378 c31a3d4d-7098-419a-a5c4-0623780f6bbf

--- document_etc/debug.bat
+++
@@ -1,3 +0,0 @@
-:Ver.1.1 作業フォルダをdebug.batの場所に変更する
-@cd /d %~dp0
-java -jar Saccubus.jar 2>&1 | java -cp Bin.jar Tee log.txt
--- vhook/src/util.h
+++
@@ -1,6 +0,0 @@
-#ifndef UTIL_H_
-#define UTIL_H_
-
-int rnd(void);
-
-#endif /*UTIL_H_*/
