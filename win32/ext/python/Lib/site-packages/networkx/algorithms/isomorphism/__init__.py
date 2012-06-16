7d100c71076d60f1de30f16ba92aa43e23730c55
ledyba <ledyba@c31a3d4d-7098-419a-a5c4-0623780f6bbf>
2012-06-16 00:49:46 +0000

    delete old python
    git-svn-id: svn+ssh://svn.osdn.jp/svnroot/saccubus/trunk@390 c31a3d4d-7098-419a-a5c4-0623780f6bbf

--- win32/ext/python/Lib/site-packages/networkx/algorithms/isomorphism/__init__.py
+++
@@ -1,6 +0,0 @@
-import networkx.algorithms.isomorphism.isomorph
-import networkx.algorithms.isomorphism.isomorphvf2
-from networkx.algorithms.isomorphism.isomorph import *
-from networkx.algorithms.isomorphism.isomorphvf2 import GraphMatcher,DiGraphMatcher
-from networkx.algorithms.isomorphism.vf2weighted import *
-
--- win32/ext/python/Lib/site-packages/networkx/linalg/__init__.py
+++
@@ -1,5 +0,0 @@
-from networkx.linalg.attrmatrix import *
-import networkx.linalg.attrmatrix
-from networkx.linalg.spectrum import *
-import networkx.linalg.spectrum
-
