28ccc13f003e88374fe1571f90ffb84d54fc0ebd
orz- <orz-@c31a3d4d-7098-419a-a5c4-0623780f6bbf>
2013-05-29 16:03:24 +0000

    1.41a ライセンス変更、readme修正、nicovideoからSDL_gfx分離（修正漏れ）
    git-svn-id: svn+ssh://svn.osdn.jp/svnroot/saccubus/branches/dev_branch@448 c31a3d4d-7098-419a-a5c4-0623780f6bbf

--- frontend/src/saccubus/MainFrame_AboutBox.java
+++ frontend/src/saccubus/MainFrame_AboutBox.java
@@ -52,8 +52,8 @@ public class MainFrame_AboutBox extends JDialog implements ActionListener {
 
 //	String version = "ver1.22r(2008/04/27)";
 
-	public static final String rev = "1.41";
-	private static final String modefied = " (2013/05/28)";
+	public static final String rev = "1.41.a";
+	private static final String modefied = " (2013/05/29)";
 
 	String productHTML =
 		"<html>" +
