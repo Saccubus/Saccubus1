c573cc50ff29cd2295c75cbe75fd1a5e2dba9dab
orz- <orz-@c31a3d4d-7098-419a-a5c4-0623780f6bbf>
2014-03-31 19:23:14 +0000

    1.48(2014/03/31)auto.bat debug.bat修正他
    git-svn-id: svn+ssh://svn.osdn.jp/svnroot/saccubus/branches/dev_branch2@483 c31a3d4d-7098-419a-a5c4-0623780f6bbf

--- frontend/src/saccubus/MainFrame_AboutBox.java
+++ frontend/src/saccubus/MainFrame_AboutBox.java
@@ -52,7 +52,7 @@ public class MainFrame_AboutBox extends JDialog implements ActionListener {
 
 //	String version = "ver1.22r(2008/04/27)";
 
-	public static final String rev = "1.48.d3";
+	public static final String rev = "1.48";
 	private static final String modefied = " (2014/03/31)";
 
 	String productHTML =
