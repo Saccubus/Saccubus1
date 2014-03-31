8e1df355952185d310e602bc7ceeee2b39e3c2dd
orz- <orz-@c31a3d4d-7098-419a-a5c4-0623780f6bbf>
2014-03-31 19:23:14 +0000

    1.48(2014/03/31)auto.bat debug.bat修正他
    git-svn-id: svn+ssh://svn.osdn.jp/svnroot/saccubus/branches/dev_branch@483 c31a3d4d-7098-419a-a5c4-0623780f6bbf

--- document_etc/auto.bat
+++ document_etc/auto.bat
@@ -1,20 +1,20 @@
 @echo off
 @cd /d %~dp0
+:Ver.1.2 Javaのパスの設定を不要にする
 echo ----------------------------------------------------
-echo 「さきゅばす」自動変換バッチファイル 1.1
+echo 「さきゅばす」自動変換バッチファイル 1.2
 echo ----------------------------------------------------
 :下記のいくつかの事項を変更して、ダブルクリック！ですよ。
 
 :終了後にシャットダウンする？YESならする、それ以外ならしない。
 set SHUTDOWN=NO
 
-:メールアドレス
+:メールアドレスとパスワード設定　ブラウザ情報共有の設定済みならそのままでOK
 set MAILADDR=doremi@mahodo.co.jp
-:パスワード
 set PASSWORD=steeki_tabetai
 
 :この行は無視して結構です。
-set CMD=java -jar Saccubus.jar %MAILADDR% %PASSWORD%
+call :default
 
 :以降、
 :%CMD% <動画ID> <過去ログ>
@@ -28,4 +28,36 @@ echo ----------------------------------------------------
 echo すべて終了しました。
 :シャットダウンする
 if %SHUTDOWN%==YES shutdown -s -t 30 -c "さきゅばす自動変換バッチファイル"
+goto :exit
+
+:default
+set local>nul
+set log=log.txt
+for %%I in (java.exe) do @set java="%%~$PATH:I"
+if not %java% == "" goto :javago
+:Java7
+set jhome=\Program Files\Java\jre7\bin;\Program Files (x86)\Java\jre7\bin;C:\Program Files\Java\jre7\bin;C:\Program Files (x86)\Java\jre7\bin
+for %%I in (java.exe) do @set java="%%~$jhome:I"
+if not %java% == "" goto :javago
+:Java8
+set jhome=\Program Files\Java\jre8\bin;\Program Files (x86)\Java\jre8\bin;C:\Program Files\Java\jre8\bin;C:\Program Files (x86)\Java\jre8\bin
+for %%I in (java.exe) do @set java="%%~$jhome:I"
+if not %java% == "" goto :javago
+goto :error
+:javago
+echo %java%
+echo %java% >>%log% 2>>&1
+%java% -version
+%java% -version >>%log% 2>>&1
+set CMD=%java% -jar Saccubus.jar %MAILADDR% %PASSWORD%
+exit /b
+
+:error
+echo java.exeが見つかりません。
+echo readmeNew.txtを参照してJavaをインストールしてパスを設定したください。
+echo.
+echo java.exeが見つかりません。>>%log%
+echo readmeNew.txtを参照してJavaをインストールしてパスを設定したください。>>%log%
+echo.
+:exit
 pause
--- document_etc/debug.bat
+++ document_etc/debug.bat
@@ -1,3 +1,37 @@
 :Ver.1.1 作業フォルダをdebug.batの場所に変更する
-@cd /d %~dp0
-java -jar Saccubus.jar 2>&1 | java -cp Bin.jar Tee log.txt
+:Ver.1.2 Javaのパスの設定を不要にする
+@echo off
+set local>nul
+cd /d %~dp0
+set log=log.txt
+
+:default
+for %%I in (java.exe) do @set java="%%~$PATH:I"
+if not %java% == "" goto :javago
+:Java7
+set jhome=\Program Files\Java\jre7\bin;\Program Files (x86)\Java\jre7\bin;C:\Program Files\Java\jre7\bin;C:\Program Files (x86)\Java\jre7\bin
+for %%I in (java.exe) do @set java="%%~$jhome:I"
+if not %java% == "" goto :javago
+:Java8
+set jhome=\Program Files\Java\jre8\bin;\Program Files (x86)\Java\jre8\bin;C:\Program Files\Java\jre8\bin;C:\Program Files (x86)\Java\jre8\bin
+for %%I in (java.exe) do @set java="%%~$jhome:I"
+if not %java% == "" goto :javago
+goto :error
+
+:javago
+echo %java%
+echo %java% >%log% 2>&1
+%java% -version
+%java% -version >>%log% 2>>&1
+:saccubusgo
+%java% -jar Saccubus.jar 2>&1 | %java% -cp Bin.jar Tee -a %log%
+goto :eof
+:error
+echo java.exeが見つかりません。
+echo readmeNew.txtを参照してJavaをインストールしてパスを設定したください。
+echo.
+echo java.exeが見つかりません。>>%log%
+echo readmeNew.txtを参照してJavaをインストールしてパスを設定したください。>>%log%
+echo.
+pause
+:eof
