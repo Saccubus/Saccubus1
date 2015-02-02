c1b836bf43aa07c8b06a829f8241674db4a4cfb8
orz- <orz-@c31a3d4d-7098-419a-a5c4-0623780f6bbf>
2015-02-02 13:36:39 +0000

    1.51 for Comment Typing
    git-svn-id: svn+ssh://svn.osdn.jp/svnroot/saccubus/branches/dev_branch2@493 c31a3d4d-7098-419a-a5c4-0623780f6bbf

--- frontend/src/saccubus/MainFrame_AboutBox.java
+++ frontend/src/saccubus/MainFrame_AboutBox.java
@@ -52,8 +52,8 @@ public class MainFrame_AboutBox extends JDialog implements ActionListener {
 
 //	String version = "ver1.22r(2008/04/27)";
 
-	public static final String rev = "1.50";
-	private static final String modefied = " (2014/05/11)";
+	public static final String rev = "1.51d2";
+	private static final String modefied = " (2015/01/12)";
 
 	String productHTML =
 		"<html>" +
