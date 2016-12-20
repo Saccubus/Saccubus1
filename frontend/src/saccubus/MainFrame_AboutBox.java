459fd2bdb8c3a5e8a6fc4318c2c6eb59be77ed85
orz <orz-@users.osdn.me>
2016-12-20 23:36:36 +0900

    1.66.3.06 Mson整理

--- frontend/src/saccubus/MainFrame_AboutBox.java
+++ frontend/src/saccubus/MainFrame_AboutBox.java
@@ -52,7 +52,7 @@ public class MainFrame_AboutBox extends JDialog implements ActionListener {
 
 //	String version = "ver1.22r(2008/04/27)";
 
-	public static final String rev = "1.66.3.05";
+	public static final String rev = "1.66.3.06";
 	private static final String modefied = " (2016/12/20)";
 
 	String productHTML =
