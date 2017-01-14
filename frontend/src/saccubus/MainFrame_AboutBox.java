534a44b86c6e164c72392d091168b16a42d02308
orz <orz-@users.osdn.me>
2017-01-14 10:55:14 +0900

    1.67.1.11 表示微修正,doc微修正

--- frontend/src/saccubus/MainFrame_AboutBox.java
+++ frontend/src/saccubus/MainFrame_AboutBox.java
@@ -52,8 +52,8 @@ public class MainFrame_AboutBox extends JDialog implements ActionListener {
 
 //	String version = "ver1.22r(2008/04/27)";
 
-	public static final String rev = "1.67.1.10";
-	private static final String modefied = " (2017/01/13)";
+	public static final String rev = "1.67.1.11";
+	private static final String modefied = " (2017/01/14)";
 
 	String productHTML =
 		"<html>" +
