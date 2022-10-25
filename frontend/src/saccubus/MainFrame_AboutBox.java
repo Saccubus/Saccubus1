bd2bd77a2464084be008c230ca726c27050a8aa9
nnn-revo2012 <nnn_revo2012@yahoo.co.jp>
2022-10-25 22:18:59 +0900

    1.67.8.10 ドキュメント修正

--- frontend/src/saccubus/MainFrame_AboutBox.java
+++ frontend/src/saccubus/MainFrame_AboutBox.java
@@ -52,8 +52,8 @@ public class MainFrame_AboutBox extends JDialog implements ActionListener {
 
 //	String version = "ver1.22r(2008/04/27)";
 
-	public static final String rev = "1.67.8.09";
-	private static final String modefied = " (2022/04/07)";
+	public static final String rev = "1.67.8.10";
+	private static final String modefied = " (2022/10/25)";
 
 	String productHTML =
 		"<html>" +
