ed0c4997e2d58437e37bbcbda8e2acac35172695
nnn-revo2012 <nnn_revo2012@yahoo.co.jp>
2023-03-09 09:35:32 +0900

    1.68.8.11 ドキュメント修正、saccubus.jarのバージョン変更

--- frontend/src/saccubus/MainFrame_AboutBox.java
+++ frontend/src/saccubus/MainFrame_AboutBox.java
@@ -52,8 +52,8 @@ public class MainFrame_AboutBox extends JDialog implements ActionListener {
 
 //	String version = "ver1.22r(2008/04/27)";
 
-	public static final String rev = "1.67.8.10";
-	private static final String modefied = " (2022/10/25)";
+	public static final String rev = "1.67.8.11";
+	private static final String modefied = " (2023/03/10)";
 
 	String productHTML =
 		"<html>" +
