a160cc4ae7fddd1b69e4cf4e5b0f63f76a529c44
nnn-revo2012 <nnn_revo2012@yahoo.co.jp>
2023-10-31 04:21:35 +0900

    1.70.0.12 - ニコ動用の設定ファイル新規追加(ニコ動HTML5_854x480.xml)   ファイル→開く(Open)でフォルダーoptionFDev/ニコ動HTML5_854x480.xmlを読み込んでください。   フォントがmeiryob.ttc、影がHtml5プレイヤー風、共通FFmpegの設定が「PC_854x480アス比共通_mp4」となります。   720pや1080pの動画にする場合は共通FFmpegの設定で選択してください。 - 共通FFmpegの設定を追加   - PC_1280x720アス比共通_mp4 PC_1920x1080アス比共通_mp4   - ニコ生_640x360アス比共通_mp4 ニコ生_800x450アス比共通_mp4   - ニコ生_1280x720@30fpsアス比共通_mp4 ニコ生_1920x1080@30fpsアス比共通_mp4 - ニコ生用の設定ファイルで上記「ニコ生_1280x720@30fpsアス比共通_mp4」を選ぶように修正 - ドキュメント修正

--- frontend/src/saccubus/MainFrame_AboutBox.java
+++ frontend/src/saccubus/MainFrame_AboutBox.java
@@ -52,8 +52,8 @@ public class MainFrame_AboutBox extends JDialog implements ActionListener {
 
 //	String version = "ver1.22r(2008/04/27)";
 
-	public static final String rev = "1.70.0.11";
-	private static final String modefied = " (2023/10/30)";
+	public static final String rev = "1.70.0.12";
+	private static final String modefied = " (2023/10/31)";
 
 	String productHTML =
 		"<html>" +
