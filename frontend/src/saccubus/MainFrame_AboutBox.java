9af24530f6e84f641d83767a80c46a5894f0f4e0
nnn-revo2012 <nnn_revo2012@yahoo.co.jp>
2023-11-06 00:26:06 +0900

    1.70.0.05a 本バージョンはニコ動新サーバー完全移行までHTTP方式で動画を取得ための暫定版です - ドキュメント修正

--- frontend/src/saccubus/MainFrame_AboutBox.java
+++ frontend/src/saccubus/MainFrame_AboutBox.java
@@ -52,8 +52,8 @@ public class MainFrame_AboutBox extends JDialog implements ActionListener {
 
 //	String version = "ver1.22r(2008/04/27)";
 
-	public static final String rev = "1.70.0.10b";
-	private static final String modefied = " (2023/10/26)";
+	public static final String rev = "1.70.0.05a";
+	private static final String modefied = " (2023/11/06)";
 
 	String productHTML =
 		"<html>" +
