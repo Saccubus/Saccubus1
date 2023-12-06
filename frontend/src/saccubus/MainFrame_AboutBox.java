77748cd905ddff67f95bb0fe9badedc6ac4b7bc4
nnn-revo2012 <nnn_revo2012@yahoo.co.jp>
2023-12-06 22:23:19 +0900

    Revert "1.67.8.16a"
    This reverts commit 7549de708437c4765f282bfd02f355c3e4676ad2.

--- frontend/src/saccubus/MainFrame_AboutBox.java
+++ frontend/src/saccubus/MainFrame_AboutBox.java
@@ -52,8 +52,8 @@ public class MainFrame_AboutBox extends JDialog implements ActionListener {
 
 //	String version = "ver1.22r(2008/04/27)";
 
-	public static final String rev = "1.67.8.20";
-	private static final String modefied = " (2023/11/15)";
+	public static final String rev = "1.67.8.16";
+	private static final String modefied = " (2023/07/06)";
 
 	String productHTML =
 		"<html>" +
